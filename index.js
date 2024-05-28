8d188ad4b3e9bbb8e7922ab8aec85c45f04894be
Victor Okon <okonvictor444@gmail.com>
2024-05-28 10:18:13 +0000

    initial commit

--- index.js
+++ index.js
@@ -18,12 +18,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+//Function to generate the timestamp
+const getTimestamp = (date)=>({
+  unix: Math.floor(date.getTime()/1000.0),
+  utc: date.toUTCString()
 });
 
+// your first API endpoint... 
+app.get("/api/", function (req, res) {
+  let currentDate = new Date()
+  res.json(getTimestamp(currentDate));
+});
+
+
+app.get("/api/:date_string?", (req, res)=>{
+  const {date_string} = req.params;
+  let date;
+
+  if (/^\d+$/.test(date_string)) {
+    date = new Date(parseInt(date_string, 10) * 1000)
+  } else {
+    date = new Date(date_string);
+  }
+
+  if(isNaN(date.getTime())){
+    res.json({error: "Invalid Date"});
+  }else{
+    res.json(getTimestamp(date));
+  }
+});
 
 
 // Listen on port set in environment variable or default to 3000
